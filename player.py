cea281b34a0d090d4d300c410c7d3fb7b9f72389
Michael Mones <kickboxer2584@yahoo.com>
2013-11-23 20:53:25 -0500

    Added Accessors + toString

--- player.py
+++ player.py
@@ -4,3 +4,26 @@ class Player:
         self.__name = playerName
         self.__cardList = []
         self.__chips = 11
+
+    # -------------------------------
+    # Accessors
+
+    def getCards(self):
+        return self.__cardList
+    
+    def getChips(self):
+        return self.__chips
+
+    def getName(self):
+        return self.__playerName
+
+    def getSortedList(self, cardList):
+        self.__cardList.sort()
+        return self.__cardList
+
+    # -------------------------------
+    # 'toString'
+
+    def __str__(self):
+        return list(self.__cardList)
+
